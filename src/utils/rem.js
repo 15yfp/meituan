05223cf2408c8e20fe883887adb56a3de814e2ef
yfp <1640261646@qq.com>
2020-08-14 14:15:50 +0800

    脚手架搭建

---
+++ src/utils/rem.js
@@ -0,0 +1,4 @@
+function font() {
+  document.documentElement.style.fontSize = document.documentElement.clientWidth / 7.5 + 'px';
+}
+window.onresize = font();
---
+++ src/views/Home/Home.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="home">
+      home
+      <h1>标题</h1>
+      <svgIcon icon-class="search" className="searchSvg"></svgIcon>
+  </div>
+</template>
+
+<script>
+
+export default {}
+
+</script>
+
+<style lang="scss">
+.home {
+  font-size: 20px;
+  color: red;
+  h1 {
+    color: yellow;
+  }
+  .searchSvg {
+    color: red;
+  }
+}
+
+</style>
